0146a34480921f7c1c659432aacba2f00262d5bd
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-09 00:49:24 +0500

    type delete

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,13 +1,16 @@
-import type { IGetMobiles } from "../types/types";
+import type { IDeleteMobiles, IGetMobiles } from "../types/types";
 import axios from "axios";
-
-
 
 const api = "https://69e5ff70ce4e908a155ec5a1.mockapi.io/users";
 
+export const getMobiles = async (search: string): Promise<IGetMobiles[]> => {
+  return (
+    await axios.get(api, {
+      params: search ? { search } : {},
+    })
+  ).data;
+};
 
-export const getMobiles = async (search:string):Promise<IGetMobiles[]>=>{
-    return (await axios.get(api,{
-        params:search?{search}:{},
-    })).data;
+export const deleteMobile = async (id: number): Promise<IDeleteMobiles> => {
+  return await axios.delete(`${api}/${id}`);
 };
