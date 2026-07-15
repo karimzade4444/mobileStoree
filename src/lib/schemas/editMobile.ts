333c8657a016570100654d80af7e97141a42cefb
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-15 22:10:26 +0500

    edit schema

---
+++ src/lib/schemas/editMobile.ts
@@ -0,0 +1,19 @@
+import * as z from "zod";
+
+export const EditMobileSchema = z.object({
+  brand: z.string().min(1, "Выберите бренд"),
+
+  name: z.string().min(1, "Введите модель"),
+
+  price: z.coerce.number().min(1, "Введите цену"),
+
+  storage: z.coerce.number().min(1, "Выберите память"),
+
+  color: z.string().min(1, "Введите цвет"),
+
+  logo: z.string().min(1, "Введите ссылку на логотип"),
+
+  title: z.string().min(1, "Введите описание"),
+});
+
+export type IEditMobileSchema = z.infer<typeof EditMobileSchema>;
