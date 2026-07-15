db0b36c0f2186a7e940dab4cdd348544b8dbec32
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-15 21:06:30 +0500

    zod

--- src/components/compons/CreateModal.tsx
+++ src/components/compons/CreateModal.tsx
@@ -7,13 +7,16 @@ import FormSelect from "../form/FormSelect";
 import FormInput from "../form/FormInput";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createUsers } from "@/lib/api/api";
+import { createMobileSchema } from "@/lib/schemas/createMobile";
 
 interface ICreateModal {
   openCreateModal: boolean;
   setOpenCreateModal: Dispatch<SetStateAction<boolean>>;
 }
 const CreateModal = ({ openCreateModal, setOpenCreateModal }: ICreateModal) => {
-  const { control, handleSubmit } = useForm<ICreateMobiles>();
+  const { control, handleSubmit, reset } = useForm<ICreateMobiles>({
+    resolver: zodResolver(createMobileSchema)
+  });
 
   const queryClient = useQueryClient();
 
@@ -23,7 +26,7 @@ const CreateModal = ({ openCreateModal, setOpenCreateModal }: ICreateModal) => {
       queryClient.invalidateQueries({
         queryKey: ["getMobiles"],
       });
-
+reset()
       setOpenCreateModal(false);
     },
   });
@@ -33,120 +36,122 @@ const onSubmit = (data: ICreateMobiles) => {
   return (
     <>
       <Dialog open={openCreateModal} onOpenChange={setOpenCreateModal}>
-        <DialogContent onSubmit={handleSubmit(onSubmit)}>
-          <DialogTitle>Добавление телефона</DialogTitle>
-          <div>
-            <p className=" text-neutral-400">Бренд</p>
-            <FormSelect
-              control={control}
-              name="brand"
-              placeholder="Выберите бренд"
-              options={[
-                {
-                  label: "Apple",
-                  value: "Apple",
-                },
-                {
-                  label: "Samsung",
-                  value: "Samsung",
-                },
-                {
-                  label: "Xiaomi",
-                  value: "Xiaomi",
-                },
-                {
-                  label: "Honor",
-                  value: "Honor",
-                },
-                {
-                  label: "Huawei",
-                  value: "Huawei",
-                },
-              ]}
-            />
-          </div>
-          <div>
-            <p className=" text-neutral-400">Модель</p>
-            <FormInput
-              control={control}
-              name="name"
-              placeholder="Ввелите модель"
-            />
-          </div>
-          <div>
-            <p className=" text-neutral-400">Цена ($)</p>
-            <FormInput
-              control={control}
-              name="price"
-              placeholder="Введите цену"
-            />
-          </div>
-          <div>
-            <p className=" text-neutral-400">Память (ГБ)</p>
-            <FormSelect
-              control={control}
-              name="storage"
-              placeholder="Выберите память"
-              options={[
-                {
-                  label: "64",
-                  value: "64",
-                },
-                {
-                  label: "128",
-                  value: "128",
-                },
-                {
-                  label: "256",
-                  value: "256",
-                },
-                {
-                  label: "512",
-                  value: "512",
-                },
-                {
-                  label: "1024",
-                  value: "1024",
-                },
-              ]}
-            />
-          </div>
-          <div>
-            <p className=" text-neutral-400">Цвет</p>
-            <FormInput
-              control={control}
-              name="color"
-              placeholder="Введите цвет"
-            />
-          </div>
-          <div>
-            <p className=" text-neutral-400">Лого</p>
-            <FormInput
-              control={control}
-              name="logo"
-              placeholder="https://..."
-            />
-          </div>
-          <div>
-            <p className=" text-neutral-400">Описание</p>
-            <FormInput
-              control={control}
-              name="title"
-              placeholder="Введите описание"
-            />
-          </div>
-          <div className=" grid grid-cols-2 gap-10">
-            <Button
-              variant="outline"
-              className="h-10"
-              onClick={() => setOpenCreateModal(false)}
-            >
-              Закрыть
-            </Button>
-            <Button variant="ghost" className="h-10" type="submit">
-              Добавить
-            </Button>
-          </div>
+        <DialogContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <DialogTitle>Добавление телефона</DialogTitle>
+            <div className="mt-5">
+              <p className=" text-neutral-400">Бренд</p>
+              <FormSelect
+                control={control}
+                name="brand"
+                placeholder="Выберите бренд"
+                options={[
+                  {
+                    label: "Apple",
+                    value: "Apple",
+                  },
+                  {
+                    label: "Samsung",
+                    value: "Samsung",
+                  },
+                  {
+                    label: "Xiaomi",
+                    value: "Xiaomi",
+                  },
+                  {
+                    label: "Honor",
+                    value: "Honor",
+                  },
+                  {
+                    label: "Huawei",
+                    value: "Huawei",
+                  },
+                ]}
+              />
+            </div>
+            <div className="mt-3">
+              <p className=" text-neutral-400">Модель</p>
+              <FormInput
+                control={control}
+                name="name"
+                placeholder="Ввелите модель"
+              />
+            </div>
+            <div className="mt-3">
+              <p className=" text-neutral-400">Цена ($)</p>
+              <FormInput
+                control={control}
+                name="price"
+                placeholder="Введите цену"
+              />
+            </div>
+            <div className="mt-3">
+              <p className=" text-neutral-400">Память (ГБ)</p>
+              <FormSelect
+                control={control}
+                name="storage"
+                placeholder="Выберите память"
+                options={[
+                  {
+                    label: "64",
+                    value: "64",
+                  },
+                  {
+                    label: "128",
+                    value: "128",
+                  },
+                  {
+                    label: "256",
+                    value: "256",
+                  },
+                  {
+                    label: "512",
+                    value: "512",
+                  },
+                  {
+                    label: "1024",
+                    value: "1024",
+                  },
+                ]}
+              />
+            </div>
+            <div className="mt-3">
+              <p className=" text-neutral-400">Цвет</p>
+              <FormInput
+                control={control}
+                name="color"
+                placeholder="Введите цвет"
+              />
+            </div>
+            <div className="mt-3">
+              <p className=" text-neutral-400">Лого</p>
+              <FormInput
+                control={control}
+                name="logo"
+                placeholder="https://..."
+              />
+            </div>
+            <div className="mt-3">
+              <p className=" text-neutral-400">Описание</p>
+              <FormInput
+                control={control}
+                name="title"
+                placeholder="Введите описание"
+              />
+            </div>
+            <div className=" grid grid-cols-2 gap-10 mt-5">
+              <Button
+                variant="outline"
+                className="h-10"
+                onClick={() => setOpenCreateModal(false)}
+              >
+                Закрыть
+              </Button>
+              <Button variant="ghost" className="h-10" type="submit">
+                Добавить
+              </Button>
+            </div>
+          </form>
         </DialogContent>
       </Dialog>
     </>
--- src/lib/schemas/createUser.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod";
-
-export const createUserSchema = z.object({
-  login: z.email().min(1, "Логин обязателен"),
-  password: z.coerce.number<number>(),
-});
-
-export type ICreateUserSchema = z.infer<typeof createUserSchema>;
